b2189b82fd7128927c2b7bab6d580f0c12a349f6
fasthill <kangeun@naver.com>
2021-08-24 22:20:43 +0900

    tour 주요 정도 class

--- Tour.py
+++ Tour.py
@@ -6,10 +6,12 @@ class TourInfo:
     area = ''
     link = ''
     img = ''
+    contents = ''
     # 생성자
-    def __init__(self, title, price, area, link, img):
+    def __init__(self, title, price, area, link, img, contents=None):
         self.title = title
         self.price = price
         self.area = area
         self.link = link
         self.img = img
+        self.contents = contents
