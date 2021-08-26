a97e5dac7c8f37a0168bbc79dda4795238d46d4c
fasthill <kangeun@naver.com>
2021-08-26 19:36:00 +0900

    page navigate driver.back() .forward()

--- selenium-tech-with-tim/page-navi-clicking-element.py
+++ selenium-tech-with-tim/page-navi-clicking-element.py
@@ -1,33 +1,41 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-from bs4 import BeautifulSoup as bs
-import requests
 
 PATH = '../selenium-Tacademy/chromedriver.exe'
 driver = webdriver.Chrome(PATH)
 
-driver.set_window_size(1024, 600)
+driver.set_window_size(1024, 700)
 
 driver.get('https://techwithtim.net')
+time.sleep(1)
 
-link = driver.find_element_by_partial_link_text('Python Programming')
-print("wait")
-time.sleep(3)
-print("in")
+link = driver.find_element_by_link_text('Python Programming')
 link.click()
-print("after click")
-time.sleep(5)
-print("out")
-soup = bs(driver.page_source, 'html.parser')
-print(soup.prettify())
-# try:
-#     element = WebDriverWait(driver, 10).until(
-#         EC.presence_of_element_located((By.LINK_TEXT, "Beginner Python Tutorials"))
-#     )
-#     element.click()
-# except:
-#     driver.quit()
+# ad pop up skip logic
+driver.back()
+driver.forward()
+# ad pop up
+
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Beginner Python Tutorials"))
+    )
+    element.click()
+
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "sow-button-19310003"))
+    )
+    element.click()
+
+    driver.back()
+    driver.back()
+    driver.back()
+    driver.forward()
+    driver.forward()
+except WebDriverWait:
+    driver.quit()
+
+driver.quit()
