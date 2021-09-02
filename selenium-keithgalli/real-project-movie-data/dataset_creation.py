0bc801a3cab9ef2cc095e47644147169a3978cc5
fasthill <kangeun@naver.com>
2021-09-02 23:13:38 +0900

    dataset creation 1st step - Part I

--- selenium-keithgalli/real-project-movie-data/dataset_creation.py
+++ selenium-keithgalli/real-project-movie-data/dataset_creation.py
@@ -22,7 +22,27 @@ info_box = soup.find('table', attrs={'class':'infobox vevent'})
 
 info_rows = info_box.find_all('tr')
 
-for row in info_rows:
-    # print('{%s} : {%s}'.format(tr.th.text, tr.td.a.text ))
+# for row in info_rows:
+#     print(row.prettify())
 
+def get_content_value(row_data):
+    if row_data.find('li'):
+        return [li.get_text(" ", strip=True).replace('\xa0', ' ') for li in row_data.find_all('li')]
+    else:
+        return row_data.get_text(" ", strip=True).replace('\xa0', ' ') # replace special characters
 
+movie_info = {}
+for index, row in enumerate(info_rows):
+    if index == 0:
+        movie_info['title'] = row.find('th').get_text(" ", strip=True)
+        # get_text change like line feed character to a space.
+        # https://www.crummy.com/software/BeautifulSoup/bs4/doc/index.html?highlight=get_text#get-text
+    elif index == 1:
+        continue
+    else:
+        content_key = row.find('th').get_text(" ", strip=True)
+        content_value = get_content_value(row.find('td'))
+        movie_info[content_key] =  content_value
+
+for key, value in movie_info.items():
+    print('{} : {} '.format(key, value))
