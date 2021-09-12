881dd0c51bbbb242751b18252ef7dbdb5fe824df
fasthill <kangeun@naver.com>
2021-09-12 12:17:07 +0900

    convert money notation to float type notation

--- selenium_keithgalli/real-project-movie-data/cleaing_data_money_exp.py
+++ selenium_keithgalli/real-project-movie-data/cleaing_data_money_exp.py
@@ -2,6 +2,7 @@
 from bs4 import BeautifulSoup as bs
 import requests
 from module_collection import get_info_box, save_data, load_data, minute_to_integer
+from module_money_conversion import money_conversion
 import json
 import re
 
@@ -21,28 +22,25 @@ money_conversion("$790,000") --> 790000
 use test_money_conversion.py to test your solution
 '''
 
+''' use from module_money_conversion.py
 def money_conversion(money):
-    pattern = re.compile(r'\$(\d{1,3}([,.]*\d{1,3})*)\s*(mil)*')
-    try:
-        result = re.search(pattern, money)
-        quantity = result.group(1)
-        quantity = quantity.replace(",","")  # delete comma in string
-        money_val = float(quantity)
-        if result.group(3) == 'mil':
-            money_val = money_val * 1_000_000
-        return f'{money_val:,}'  # insert comma for thousand separator
-    except:
-        return 'None'
+    pass
+'''
 
-#$30.9 million est. ₹ 79.43 crore (US$11 million)  ₹ 83 crore ['est.', '₹2,024–2,100 crore', '(', '$311–340\xa0million', ')']
-# $7.7
-#  million
-
-money_box_office = [mv.get('Box office', 'None') for mv in movie_info_list]
+''' use test_money_conversion.py to test cases
+$30.9 million est. ₹ 79.43 crore (US$11 million)  ₹ 83 crore ['est.', '₹2,024–2,100 crore', '(', '$311–340\xa0million', ')']
+$7.7
+ million
+'''
 
 for mv in movie_info_list:
-    mbo = mv.get('Box office', 'N/A')
-    result = money_conversion(mbo)
-    mv['Box office (convert)'] = result
+    money_budget = mv.get('Budget', 'N/A')
+    money_box_office = mv.get('Box office', 'N/A')
+    mv['Budget (convert)'] = money_conversion(money_budget)
+    mv['Box office (convert)'] = money_conversion(money_box_office)
+
+t1 = 'Beauty and the Beast'
+aa = [i for i, sub in enumerate(movie_info_list) if sub['title'] == t1]
+print (aa)
 
 save_data('data/disney_datam.json', movie_info_list)
