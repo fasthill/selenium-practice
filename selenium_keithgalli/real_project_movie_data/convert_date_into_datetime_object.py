1d111a2b8701eed099c7c44c6852044f6d0e71b0
fasthill <kangeun@naver.com>
2021-09-16 11:51:31 +0900

    date format conversion, modify re, split defs for convenience

--- selenium_keithgalli/real_project_movie_data/convert_date_into_datetime_object.py
+++ selenium_keithgalli/real_project_movie_data/convert_date_into_datetime_object.py
@@ -1,14 +1,13 @@
-# Convert datas into datetime objects
-from module_collection import get_info_box, save_data, load_data, save_data_pickle, minute_to_integer
+# Convert data into datetime objects
+from module_collection import  load_data, save_data_pickle
 from module_datetime_conversion import date_conversion
 import pickle
 import json
 import re
 
 movie_info_list = load_data('data/disney_datam.json')
-dates = [date.get('Release date','N/A') for date in movie_info_list]
-
-date_str = [date_conversion(date) for date in dates]
+# dates = [date.get('Release date','N/A') for date in movie_info_list]
+# date_str = [date_conversion(date) for date in dates]
 
 # for movie in movie_info_list:
 movie_info_list_temp = []
@@ -19,5 +18,3 @@ for movie in movie_info_list:
 
 save_data_pickle('data/disney_datad.pkl', movie_info_list_temp)
 # save_data('data/disney_datad.json', movie_info_list_temp)  # can't write datetime object into json.
-
-print('1')
--- selenium_keithgalli/real_project_movie_data/module_datetime_conversion.py
+++ selenium_keithgalli/real_project_movie_data/module_datetime_conversion.py
@@ -1,30 +1,34 @@
 from datetime import datetime
 import re
 
-date = ['May 19, 1937']
-
 month_list = ['January', 'February', 'March', 'April', 'May', 'June',
               'July', 'August', 'September', 'October', 'November', 'December']
 month_re = r'(January|February|March|April|May|June|July|August|September|October|November|December)'
 date_re = r'\s(\d{1,2})\,'
+date_re2 = r'\d{1,2}\s'
 year_re = r'\s(\d{4})'
 
-datetime_re = rf'{month_re}{date_re}{year_re}'
+date_re_1 = rf'{month_re}{date_re}{year_re}'
+date_re_2 = rf'{date_re2}{month_re}{year_re}'
+date_re_3 = rf'{month_re}{year_re}'
+date_re_4 = r'\d{4}'
+datetime_re = [date_re_1, date_re_2, date_re_3, date_re_4]
 
+def arange_date_string(date):  # data
+    for d_re in datetime_re:
+        try:
+            i_date = re.search(d_re, date).group()
+            return i_date
+        except:
+            pass
+    return None
+    
+    
 # delete unnecessary portion with '()' with in Release date expression list
 def clean_date(date):
-    return date.split("(")[0].strip()
+    c_date = date.split("(")[0].strip()
+    return c_date.replace('\xa0', ' ')
 
-def relocate_date_format(date):  # June 1963, 1964, => June 1, 1963, January 1, 1964
-    date = date.replace('\xa0', ' ')
-    date_s = date.split(' ')
-    if len(date_s) == 1:
-        m_date = 'January' + ' 1, ' + date_s[0]
-        return m_date
-    if len(date_s) == 2 :
-        m_date = date_s[0] + ' 1, ' + date_s[1]
-        return m_date
-    return date
 
 def date_conversion(date):
     if isinstance(date, list):
@@ -32,34 +36,34 @@ def date_conversion(date):
     if date == 'N/A':
         return None
     c_date = clean_date(date)
-    m_date = relocate_date_format(c_date)
-    return datetime_conversion(m_date)
+    return datetime_conversion(c_date)
+
 
 def month_to_num(month):
     montonum = {'January': '01', 'February': '02', 'March': '03', 'April': '04', 'May': '05', 'June': '06',
               'July': '07', 'August': '08', 'September': '09', 'October': '10', 'November': '11', 'December': '12'}
     return montonum[month]
 
+
 def change_date_to_num(i_date):
     mon = month_to_num(i_date.group(1))
     return mon+'-'+i_date.group(2)+'-'+i_date.group(3)
 
-def datetime_conversion(date):
-    if type(date) == list:
-        for idate in date:
-            i_date = re.search(datetime_re, idate)
-    else:
-        i_date = re.search(datetime_re, date)
 
-    # return datetime.strptime(change_date_to_num(i_date), '%m-%d-%Y')  # for this, needed change to num.
-    fmts = ['%B %d, %Y', '%d %B %Y']
+def datetime_conversion(date):
+    i_date = arange_date_string(date)
+
+    fmts = ['%B %d, %Y', '%d %B %Y', '%B %Y', '%Y']
     for fmt in fmts:
         try:
-            return datetime.strptime(i_date.group(), fmt)  # for this, no need to change to num. use %B
+            return datetime.strptime(i_date, fmt)  # for this, no need to change to num. use %B
         except:
             pass
 
+    return None
+
 
 if __name__ == '__main__':
-    dd = datetime_conversion(date)
-    print('datetime_c')
+    date = ['May 19, 1937']
+    dd = date_conversion(date)
+    print('date ', date, ': datetime ', dd)
