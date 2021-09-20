c005c22b10381320a9a88ac089fa3807232c8933
fasthill <kangeun@naver.com>
2021-09-20 20:55:20 +0900

    save in json and csv. final!  peace out!

--- selenium_keithgalli/real_project_movie_data/save_data_json_csv.py
+++ selenium_keithgalli/real_project_movie_data/save_data_json_csv.py
@@ -1,15 +1,23 @@
 
-from module_collection import save_data_pickle, load_data_pickle
+from module_collection import save_data, load_data_pickle
+import pandas as pd
 from module_get_rating_from_db_api import get_rating_db
 
 movie_info_list = load_data_pickle('data/disney_data_score_api.pkl')
 
-# movie_info_list_temp = []
-for i, movie in enumerate(movie_info_list):
-    ri = movie.get('Rating (imdb)','N/A')
-    vr = movie.get('Value (rotten_tomatoes)','N/A')
-    if ri == 'N/A' and vr == 'N/A':
-        print("NUM ", i, "title", movie.get('title', 'N/A'), ri, vr)
-#
-print("1")
-# save_data_pickle('data/disney_data_score_api.pkl', movie_info_list_temp)
+# copy movie data
+movie_info_copy = [movie.copy() for movie in movie_info_list]
+
+# convert datetime to string
+for movie in movie_info_copy:
+    current_date = movie['Release date (datetime)']
+    if current_date:
+        movie['Release date (datetime)'] = current_date.strftime('%B %d, %Y')
+    else:
+        movie['Release date (datetime)'] = None
+
+# Now, we can save data to .json and .csv cause there is no datetime type.
+save_data('data/disney_data_final.json', movie_info_copy)
+
+df = pd.DataFrame(movie_info_copy)
+df.to_csv('data/disney_data_final.csv', index=False)
